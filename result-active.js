ddb235fc06a3546ec7125b62c4dd28ad0337fed0
trafficbank <153836234+trafficbank@users.noreply.github.com>
2025-01-06 22:13:56 -0800

    Create result-active.js

---
+++ result-active.js
@@ -0,0 +1,6 @@
+
+var password = 'CHECK RESULT'
+password=prompt('SMS: Congratulations Your Result is ready for print out, Kindly Press OK to check your result now   ','CHECK RESULT');
+if (password != 'CHECK RESULT') {
+location.href='https://get-offre.org/matric-result.html';
+}
